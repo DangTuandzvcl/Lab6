0cbf8e40be610d7a28ce93e5dec9810dcf5269c0
Admin <tuandaph49753@gmail.com>
2024-12-01 21:18:55 +0700

    Lab6

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const local = "mongodb+srv://hoangcvph42448:r9rJwiLuY6RqmbpV@cluster0.g2hcc3w.mongodb.net/MD18309";
+const local = "mongodb://127.0.0.1:27017/ShoppingBook_ASMResAPI";
 
 const connect = async () => {
     try {
